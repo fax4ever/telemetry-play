728fbd126c846627a0f7b29625ea485ca4f95b52
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-11-02 16:46:18 +0100

    Try to avoid okhttp dep

--- sdk-autoconfigure-exporter/src/main/java/fax/play/OTelConfig.java
+++ sdk-autoconfigure-exporter/src/main/java/fax/play/OTelConfig.java
@@ -1,10 +1,14 @@
 package fax.play;
 
 import java.util.Collections;
+import java.util.HashMap;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.opentelemetry.api.GlobalOpenTelemetry;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.autoconfigure.AutoConfiguredOpenTelemetrySdk;
 
 public class OTelConfig {
@@ -12,11 +16,21 @@ public class OTelConfig {
    private final OpenTelemetry openTelemetry;
 
    public OTelConfig() {
+      HashMap<String, String> properties = new HashMap<>();
+      properties.put("otel.traces.exporter", "otlp");
+      properties.put("otel.metrics.exporter", "none");
+      properties.put("otel.service.name", "fan");
+      properties.put("otel.exporter.otlp.endpoint", "http://localhost:4317");
+
+      ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 4317).usePlaintext().build();
+      OtlpGrpcSpanExporter otlpGrpcExporter =
+            OtlpGrpcSpanExporter.builder()
+                  .setChannel(managedChannel)
+                  .build();
 
       openTelemetry = AutoConfiguredOpenTelemetrySdk.builder()
-            // At the moment we don't export Infinispan server metrics with OpenTelemetry,
-            // so we manually disable any metrics export
-            .addPropertiesSupplier(() -> Collections.singletonMap("otel.metrics.exporter", "none"))
+            .addPropertiesSupplier(() -> properties)
+            .addSpanExporterCustomizer((spanExporter, configProperties) -> otlpGrpcExporter)
             .build()
             .getOpenTelemetrySdk();
    }
